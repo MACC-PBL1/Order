354c3031594e44ec21fd790cc9f0982cba460197
enaut-genua <enaut.genua@alumni.mondragon.edu>
2025-10-26 18:37:50 +0100

    Try fix listener registering issue

--- src/order/messaging/__init__.py
+++ src/order/messaging/__init__.py
@@ -4,12 +4,14 @@ from .global_vars import (
     PUBLISHING_QUEUES,
     RABBITMQ_CONFIG,
 )
+from . import events
 from typing import (
     List,
     LiteralString,
 )
 
 __all__: List[LiteralString] = [
+    "events",
     "LISTENING_QUEUES",
     "PUBLIC_KEY",
     "PUBLISHING_QUEUES",
