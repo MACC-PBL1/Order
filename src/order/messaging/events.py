f6177b7cad3e6c3ef65fc3310038197bcc630cf2
enaut-genua <enaut.genua@alumni.mondragon.edu>
2025-11-29 01:21:48 +0100

    Fix auth name and endpoint

--- src/order/messaging/events.py
+++ src/order/messaging/events.py
@@ -232,12 +232,12 @@ def public_key(message: MessageType) -> None:
     )
 
     consul = ConsulClient(logger)
-    auth_base_url = consul.get_service_url("auth-service")
+    auth_base_url = consul.get_service_url("auth")
     if not auth_base_url:
         logger.error("The auth service couldn't be found")
         return
 
-    target_url = f"{auth_base_url}/auth-service/key"
+    target_url = f"{auth_base_url}/auth/key"
 
     # auth_base_url = "http://auth:8000"
     # target_url = f"{auth_base_url}/auth/key"
